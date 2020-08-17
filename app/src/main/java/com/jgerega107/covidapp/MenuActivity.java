8e769dc22d1f6cbfc69704bd077f08077856bb50
jgerega107 <jakegerega107@gmail.com>
2020-08-16 21:49:47 -0400

    Added some button functionality and empty activities

--- app/src/main/java/com/jgerega107/covidapp/MenuActivity.java
+++ app/src/main/java/com/jgerega107/covidapp/MenuActivity.java
@@ -1,8 +1,13 @@
 package com.jgerega107.covidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        TextView welcomeText = findViewById(R.id.welcome_text);
+        welcomeText.setText("Welcome, " + currentUser.getDisplayName() + "!");
+    }
+
+    public void toAboutActivity(View view){
+        startActivity(new Intent(getApplicationContext(), AboutActivity.class));
+    }
+
+    public void toChecklistActivity(View view){
+        startActivity(new Intent(getApplicationContext(), ChecklistActivity.class));
     }
 }
