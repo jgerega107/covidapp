5cd8a7683ad5f8f69cd1d5d2fece7cf22bce52a3
Kevin <kh220kh@gmail.com>
2020-08-17 12:43:58 -0400

    fixed checkmarks, switch statement hell

--- app/src/main/java/com/jgerega107/covidapp/ChecklistActivity.java
+++ app/src/main/java/com/jgerega107/covidapp/ChecklistActivity.java
@@ -12,36 +12,94 @@ import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-public class ChecklistActivity extends AppCompatActivity implements View.OnClickListener {
+public class ChecklistActivity extends AppCompatActivity {
+
+    Animation blink;
+    ImageView fever;
+    ImageView cough;
+    ImageView breath;
+    ImageView fatigue;
+    ImageView muscle;
+    ImageView headache;
+    ImageView taste;
+    ImageView sore;
+    ImageView con;
+    ImageView vomit;
+    ImageView dir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checklist);
+        blink = AnimationUtils.loadAnimation(this, R.anim.blink);
+        fever = (ImageView) findViewById(R.id.fever_img);
+        cough = (ImageView) findViewById(R.id.cough_img);
+        breath = (ImageView) findViewById(R.id.breath_img);
+        fatigue = (ImageView) findViewById(R.id.fatigue_img);
+        muscle = (ImageView) findViewById(R.id.muscle_img);
+        headache = (ImageView) findViewById(R.id.headache_img);
+        taste = (ImageView) findViewById(R.id.taste_img);
+        sore = (ImageView) findViewById(R.id.sore_img);
+        con = (ImageView) findViewById(R.id.con_img);
+        vomit = (ImageView) findViewById(R.id.vomit_img);
+        dir = (ImageView) findViewById(R.id.dir_img);
 
-        final Animation blink = AnimationUtils.loadAnimation(this, R.anim.blink);
-        ImageView fever = (ImageView) findViewById(R.id.fever_img);
-        ImageView cough = (ImageView) findViewById(R.id.cough_img);
-        ImageView breathe = (ImageView) findViewById(R.id.breath_img);
-        fever.setOnClickListener((View.OnClickListener) this);
-        cough.setOnClickListener((View.OnClickListener) this);
-        breathe.setOnClickListener((View.OnClickListener) this);
+        //fever.setOnClickListener((View.OnClickListener) this);
+        //cough.setOnClickListener((View.OnClickListener) this);
+        //breathe.setOnClickListener((View.OnClickListener) this);
 
         //final GridLayout symptomGrid = (GridLayout) findViewById(R.id.symptomGrid);
         //final int count = symptomGrid.getChildCount();
         //final ImageView fever = (ImageView) findViewById(R.id.fever_img);
         //fever.setOnClickListener(new View.OnClickListener() {
     }
-    @Override
-    public void onClick(View v){
+    public void onClicked(View v){
         switch (v.getId()) {
                 case R.id.fever_img:
-                    ImageView fever = (ImageView) findViewById(R.id.fever_img);
-                    final Animation blink = AnimationUtils.loadAnimation(this, R.anim.blink);
                     fever.startAnimation(blink);
                     fever.setImageResource(R.drawable.ic_baseline_check_24);
-
-            }
+                    break;
+                case R.id.cough_img:
+                    cough.startAnimation(blink);
+                    cough.setImageResource(R.drawable.ic_baseline_check_24);
+                    break;
+                case R.id.breath_img:
+                    breath.startAnimation(blink);
+                    breath.setImageResource(R.drawable.ic_baseline_check_24);
+                    break;
+                case R.id.fatigue_img:
+                    fatigue.startAnimation(blink);
+                    fatigue.setImageResource(R.drawable.ic_baseline_check_24);
+                    break;
+                case R.id.muscle_img:
+                    muscle.startAnimation(blink);
+                    muscle.setImageResource(R.drawable.ic_baseline_check_24);
+                    break;
+                case R.id.headache_img:
+                    headache.startAnimation(blink);
+                    headache.setImageResource(R.drawable.ic_baseline_check_24);
+                    break;
+                case R.id.taste_img:
+                    taste.startAnimation(blink);
+                    taste.setImageResource(R.drawable.ic_baseline_check_24);
+                    break;
+                case R.id.sore_img:
+                    sore.startAnimation(blink);
+                    sore.setImageResource(R.drawable.ic_baseline_check_24);
+                    break;
+                case R.id.con_img:
+                    con.startAnimation(blink);
+                    con.setImageResource(R.drawable.ic_baseline_check_24);
+                    break;
+                case R.id.vomit_img:
+                    vomit.startAnimation(blink);
+                    vomit.setImageResource(R.drawable.ic_baseline_check_24);
+                    break;
+                case R.id.dir_img:
+                    dir.startAnimation(blink);
+                    dir.setImageResource(R.drawable.ic_baseline_check_24);
+                    break;
+        }
 
 
         }
